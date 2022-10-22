5982065c5e0731d151150286843b8012c9ec2b6d
ksergeyru <i@ksergey.ru>
2022-10-22 17:42:34 +0300

    Пример кода с логарифмической сложностью

--- Day01_Intro/Program.cs
+++ Day01_Intro/Program.cs
@@ -11,3 +11,19 @@ void What()
 }
 
 #endregion
+
+#region y = log(n)
+int BinarySearch(int[] array, int find)
+{
+  int left = 0;
+  int right = array.Length - 1;
+  while (left <= right)
+  {
+    int middle = (left + right) / 2;
+    if (find == array[middle]) return middle;
+    else if (find < array[middle]) right = middle - 1;
+    else left = middle + 1;
+  }
+  return -1;
+}
+#endregion
